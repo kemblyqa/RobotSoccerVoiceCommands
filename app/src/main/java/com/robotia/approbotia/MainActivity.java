551b89b65d66bc552c2cc6cf08b383cf0015120d
Kem <kquirosaraya@gmail.com>
2019-05-02 20:26:36 -0600

    Primer activity para activación de bluetooth

--- app/src/main/java/com/robotia/approbotia/MainActivity.java
+++ app/src/main/java/com/robotia/approbotia/MainActivity.java
@@ -1,13 +1,28 @@
 package com.robotia.approbotia;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public static int REQUEST_BLUETOOTH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }

5ce5cdcaa813171e966ace64608f290d81582e64
Kem <kquirosaraya@gmail.com>
2019-05-03 12:00:51 -0600

    Descubrir dispositivos cercanos

--- app/src/main/java/com/robotia/approbotia/MainActivity.java
+++ app/src/main/java/com/robotia/approbotia/MainActivity.java
@@ -1,12 +1,25 @@
 package com.robotia.approbotia;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button findArduinoDevice;
+    private Button findHeadsetDevice;
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     public static int REQUEST_BLUETOOTH = 1;
 
@@ -15,14 +28,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findArduinoDevice = findViewById(R.id.firstDeviceBtn);
+        findHeadsetDevice = findViewById(R.id.secondDeviceBtn);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 1) if(resultCode == RESULT_OK) bluetoothAdapter.startDiscovery();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void selectDeviceAlertDialog(String title, final String[] devices){
+        new AlertDialog.Builder(MainActivity.this)
+            .setTitle(title)
+            .setItems(devices, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, devices[which], Toast.LENGTH_SHORT).show();
+                }
+            })
+            .show();
+    }
+
+    public void selectDevice(View view){
+        String[] devices = {"horse", "cow", "camel", "sheep", "goat"};
+        selectDeviceAlertDialog("Connection", devices);
+    }
+
+    public void requestForBluetoothService(View view){
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH);
+        } else {
+
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
